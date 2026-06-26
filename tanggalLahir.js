c026aeeb427dd0e0f21f844001f423c813aa0c2b
lrb15 <robbygo2015@gmail.com>
2026-06-26 15:23:22 +0000

    selesai mengerjakan tugas tanggalLahir

---
+++ tanggalLahir.js
@@ -0,0 +1,53 @@
+let tanggal;
+let bulan;
+let tahun;
+
+function tampilkanTanggalLahir(tgl,bln,thn) {
+    switch (bln) {
+        case 1 :
+            bulan = "Januari";
+            break;
+        case 2 :
+            bulan = "Februari";
+            break;
+        case 3 :
+            bulan = "Maret";
+            break;
+        case 4 :
+            bulan = "April";
+            break;
+        case 5 :
+            bulan = "Mei";
+            break;
+        case 6 :
+            bulan = "Juni";
+            break;
+        case 7 :
+            bulan = "Juli";
+            break;
+        case 8 :
+            bulan = "Agustus";
+            break;
+        case 9 :
+            bulan = "September";
+            break;
+        case 10 :
+            bulan = "Oktober";
+            break;
+        case  11 :
+            bulan = "November";
+            break;
+        case 12 :
+            bulan = "Desember";
+            break;       
+    }
+    console.log(tgl, bulan, thn);
+}
+
+tanggal = 14;
+bulan = 3;
+tahun = 2077;
+
+tampilkanTanggalLahir(tanggal, bulan, tahun)
+
+

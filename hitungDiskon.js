035724cf0c614309f88930cf721f73713caa0691
lrb15 <robbygo2015@gmail.com>
2026-06-26 12:54:02 +0000

    perbaikan kecil pada hitungDiskon.js

--- hitungDiskon.js
+++ hitungDiskon.js
@@ -1,27 +1,28 @@
 function hargaSetelahDiskon(harga, diskon = 0) {
-    if (harga <= 0) {
-        return 0
-    }
+    if (harga <= 0) return 0;
     return harga - (diskon/100) * harga;
 }
 
 const nasiGoreng = 25000;
 const mieGoreng = 22000;
 const capcay = 32000;
-let discount;
+
+let diskon;
 let pembayaran;
 let total;
+let setelahDiskon;
 
 console.log("Harga Nasi Goreng = Rp.", nasiGoreng);
 console.log("Harga Mie Goreng = Rp.", mieGoreng);
 console.log("Harga Capcay = Rp.", capcay);
 
 total = nasiGoreng + mieGoreng + capcay;
-discount = 10;
+diskon = 10;
+setelahDiskon = hargaSetelahDiskon(total,diskon)
 pembayaran = 100000;
 
 console.log("Harga total = Rp.", total);
-console.log("Diskon = Rp. " + discount + "%" );
-console.log("Harga setelah diskon = Rp.", hargaSetelahDiskon(total,discount));
+console.log("Diskon = Rp. " + diskon + "%" );
+console.log("Harga setelah diskon = Rp.", );
 console.log("Pembayaran = Rp.", pembayaran);
 console.log("Kembalian = Rp.", pembayaran - total);
